85a2a816d89948838f18c13d9e3a44ece60a4d35
Henrique Lopes <henrique.henriquelopess@gmail.com>
2020-09-24 23:03:23 -0300

    Adicionando cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 /* eslint-disable linebreak-style */
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/index';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
